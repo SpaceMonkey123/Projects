4e82678e4480d763d7a78a535398ab7cae022bca
Andrew Aper <49007836+SpaceMonkey123@users.noreply.github.com>
2019-03-27 19:07:47 -0500

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,11 @@
+// MyFirstProject
+public class Introduction {
+	public static void main(String[] args) {
+		// Displays personal introduction.
+		System.out.println("My name is Andrew Aper.");
+		System.out.println("My favorite color is Green.");
+		System.out.println("If I won the Lottery, I would pay off my debts and make wise investments in mutiple assets.");
+
+	}
+
+}
